f75bd72d2d283a27a138083923d465f02c175970
Luca Baldini <luca.baldini@pi.infn.it>
2022-07-08 14:14:34 +0200

    Cleanup.

--- baldaquin/buf.py
+++ baldaquin/buf.py
@@ -16,39 +16,167 @@
 """Data buffering.
 """
 
-from collections import deque
+import collections
+import enum
 import os
+import queue
 
 from loguru import logger
 
+from baldaquin.profile import timing
 
-class CircularBuffer(deque):
 
+class BufferWriteMode(enum.Enum):
+
+    """Enum for the mode in which the output file is opened.
     """
+
+    BINARY : str = 'b'
+    TEXT: str = 't'
+
+
+
+class BufferBase:
+
+    """Base class for a data buffer.
+
+    This is defining the three basic primitives, i.e., ``put_item()``, ``pop_item()``
+    and ``num_items()``, and provides a working implementation of file I/O.
+
+    Arguments
+    ---------
+    file_path : str
+        The path to the output file.
+
+    mode : BufferWriteMode
+        The file write mode.
     """
 
-    def __init__(self, file_path : str, max_length : int = None):
+    def __init__(self, file_path : str, mode : BufferWriteMode = BufferWriteMode.TEXT) -> None:
+        """Constructor.
         """
-        """
-        super().__init__([], max_length)
         self.file_path = file_path
+        self._mode = mode
+        if os.path.exists(self.file_path):
+            logger.warning(f'Output file {file_path} exists and will be overwritten')
+        open(self.file_path, f'w{self._mode.value}').close()
 
-    def create_file(self):
+    def put_item(self):
+        """Put an item into the buffer (to be reimplemented in derived classes).
         """
-        """
-        logger.info(f'Wiping out {self.file_path}')
-        with open(self.file_path, 'w') as output_file:
-            pass
+        raise NotImplementedError
 
-    def write(self):
+    def pop_item(self):
+        """Pop an item from the buffer (to be reimplemented in derived classes).
+
+        .. note::
+            The specific semantic of `which` item is returned (e.g, the first,
+            last, or something more clever) is delegated to the concrete classes,
+            but we will be mostly dealing with FIFOs, i.e., unless otherwise
+            stated it should be understood that this is popping items from the
+            left of the queue.
         """
+        raise NotImplementedError
+
+    def num_items(self):
+        """Return the number of items in the buffer (to be reimplemented in derived classes).
         """
-        if not len(self):
+        raise NotImplementedError
+
+    @timing
+    def flush(self) -> int:
+        """Write the content of the buffer to file and returns the number of
+        objects written to disk.
+
+        .. note::
+           This will write all the items in the buffer at the time of the
+           function call, i.e., items added while writing to disk will need to
+           wait for the next call.
+
+        Returns
+        -------
+        The number of objects written to disk. : int
+        """
+        size = self.num_items()
+        if not size:
             return
-        logger.debug(f'Writing {len(self)} packets to {self.file_path}...')
+        logger.debug(f'Writing {size} packets to {self.file_path}...')
         with open(self.file_path, 'a') as output_file:
-            while True:
-                try:
-                    output_file.write(f'{self.popleft()}\n')
-                except IndexError:
-                    break
+            for i in range(size):
+                output_file.write(f'{self.pop_item()}\n')
+
+
+
+class FIFO(queue.Queue, BufferBase):
+
+    """Implementation of a FIFO.
+
+    This is using the `queue <https://docs.python.org/3/library/queue.html>`_
+    module in the Python standard library.
+    """
+
+    def __init__(self, file_path : str, mode : BufferWriteMode = BufferWriteMode.TEXT,
+                 max_size : int = None) -> None:
+        """Constructor.
+        """
+        # From the stdlib documentation: maxsize is an integer that sets the
+        # upperbound limit on the number of items that can be placed in the queue.
+        # If maxsize is less than or equal to zero, the queue size is infinite.
+        if max_size is None:
+            max_size = -1
+        super().__init__(max_size)
+        BufferBase.__init__(self, file_path, mode)
+
+    def put_item(self, item):
+        """Overloaded method.
+        """
+        self.put(item)
+
+    def pop_item(self):
+        """Overloaded method.
+        """
+        return self.get()
+
+    def num_items(self):
+        """Overloaded method.
+        """
+        return self.qsize()
+
+
+
+class CircularBuffer(collections.deque, BufferBase):
+
+    """Implementation of a simple circular buffer.
+
+    This is a simple subclass of the Python
+    `collections.deque <https://docs.python.org/3/library/collections.html#collections.deque>`_
+    data structure, adding I/O facilities on top of the base class.
+
+    Verbatim from the Python documentation: `deques support thread-safe,
+    memory efficient appends and pops from either side of the deque with
+    approximately the same O(1) performance in either direction.`
+    For completeness, the idea of using a deque to implement a circular buffer
+    comes from https://stackoverflow.com/questions/4151320
+    """
+
+    def __init__(self, file_path : str, mode : BufferWriteMode = BufferWriteMode.TEXT,
+                 max_size : int = None) -> None:
+        """Constructor.
+        """
+        super().__init__([], max_size)
+        BufferBase.__init__(self, file_path, mode)
+
+    def put_item(self, item):
+        """Overloaded method.
+        """
+        self.append(item)
+
+    def pop_item(self):
+        """Overloaded method.
+        """
+        return self.popleft()
+
+    def num_items(self):
+        """Overloaded method.
+        """
+        return len(self)
--- docs/buf.rst
+++ docs/buf.rst
@@ -1,6 +1,24 @@
 :mod:`baldaquin.buf` --- Event buffering
 ========================================
 
+The module provides a :class:`BufferBase <baldaquin.buf.BufferBase>` abstract
+base class for data buffer, as well as a number of concrete classes:
+
+* :class:`FIFO <baldaquin.buf.FIFO>`
+* :class:`CircularBuffer <baldaquin.buf.CircularBuffer>`
+
+The base class defines three basic primitives---``put_item()``, ``pop_item()`` and
+``num_items()``, with obvious meanings, that need to be reimplemented in
+concrete, derived classes. The implementation should be thread-safe, as a
+buffer, by definition, will be accessed from multiple threads.
+
+In addition, the base class provide a :meth:`write() <baldaquin.buf.BufferBase.write()>`
+method that can dumps the current content of the buffer to file.
+
+The basic semantic for the object is that one will have a thread calling
+``buffer.put_item()`` any time there are data available, and another thread is
+periodically calling ``flush()`` to get the data to disk.
+
 
 Module documentation
 --------------------
