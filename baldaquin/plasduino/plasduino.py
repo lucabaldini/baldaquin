a7b6dda38d35f738b83358e4acd0960fbf792d63
Luca Baldini <luca.baldini@pi.infn.it>
2024-12-24 17:23:22 +0100

    Small refactoring.

--- baldaquin/plasduino/plasduino.py
+++ baldaquin/plasduino/plasduino.py
@@ -17,15 +17,15 @@
 """
 
 import time
+from typing import Any
 
 from baldaquin import logger
 from baldaquin import plasduino
 from baldaquin.buf import CircularBuffer
 from baldaquin.event import EventHandlerBase
-from baldaquin.plasduino.protocol import PlasduinoSerialInterface, AnalogReadout,\
-    DigitalTransition
+from baldaquin.plasduino.protocol import Marker, OpCode, AnalogReadout, DigitalTransition
 from baldaquin.runctrl import RunControlBase
-from baldaquin.serial_ import list_com_ports
+from baldaquin.serial_ import list_com_ports, SerialInterface
 
 
 
@@ -60,6 +60,113 @@ def autodetect_arduino_board() -> str:
     if len(ports) > 1:
         logger.warning('More than one arduino board found, picking the first one...')
     return ports[0].device
+
+
+
+class PlasduinoSerialInterface(SerialInterface):
+
+    """Specialized plasduino serial interface.
+
+    This is derived class of our basic serial interface, where we essentially
+    implement the simple plasduino communication protocol.
+    """
+
+    # pylint: disable=too-many-ancestors
+
+    def read_and_unpack(self, fmt: str) -> Any:
+        """Overloaded function.
+
+        For some reason on the arduino side we go into the trouble of reverting the
+        native bit order and we transmit things as big endian---I have no idea
+        what I was thinking, back in the days, but I don't think it makes sense
+        to fix this nonsense, now. Extra work on the transmitting side, and extra
+        work on the receiving side too. Good job, Luca!
+        """
+        return super().read_and_unpack(f'>{fmt}')
+
+    def wait_rem(self) -> None:
+        """
+        """
+        logger.info(f'Waiting for the run-end marker...')
+        end_mark = self.read_and_unpack('B')
+        if not end_mark == Marker.RUN_END_MARKER.value:
+            raise RuntimeError(f'End run marker mismatch, got {hex(end_mark)}.')
+        logger.info('Got it!')
+
+    def read_until_rem(self, timeout: float = None) -> None:
+        """Read data from the serial port until the end-of-run marker is found.
+        """
+        logger.info(f'Scanning serial input for run-end marker...')
+        previous_timeout = self.timeout
+        if timeout != self.timeout:
+            self.timeout = timeout
+            logger.debug(f'Serial port timeout temporarily set to {self.timeout} s...')
+        data = self.read_until(struct.pack('B', Marker.RUN_END_MARKER.value))
+        if len(data) > 0:
+            logger.debug(f'{len(data)} byte(s) found: {data}')
+        if previous_timeout != self.timeout:
+            self.timeout = previous_timeout
+            logger.debug(f'Serial port timeout restored to {self.timeout} s...')
+
+    def write_opcode(self, opcode: OpCode) -> int:
+        """Write the value of a given opcode to the serial port.
+
+        This is typically meant to signal the start/stop run, or to configure the
+        behavior of the sketch on the arduino side (e.g., select the pins for
+        analog readout).
+        """
+        logger.debug(f'Writing {opcode} to the serial port...')
+        return self.pack_and_write(opcode.value, 'B')
+
+    def write_start_run(self) -> int:
+        """ Write a start run command to the serial port.
+        """
+        return self.write_opcode(OpCode.OP_CODE_START_RUN)
+
+    def write_stop_run(self) -> int:
+        """ Write a stop run command to the serial port.
+        """
+        return self.write_opcode(OpCode.OP_CODE_STOP_RUN)
+
+    def write_cmd(self, opcode: OpCode, value: int, fmt: str) -> None:
+        """ Write a command to the arduino board.
+
+        This implies writing the opcode to the serial port, writing the actual
+        payload and, finally, reading back the arduino response and making
+        sure the communication went fine.
+
+        And, looking back at this after many years, I cannot help noticing that
+        it looks a little bit funny, but I guess it did make sense, back in the
+        days.
+
+        Arguments
+        ---------
+        opcode : OpCode
+            The opcode defining the command.
+
+        value : int
+            The actual value.
+
+        fmt : str
+            The format string.
+        """
+        self.write_opcode(opcode)
+        logger.debug(f'Writing configuration value {value} to the serial port')
+        self.pack_and_write(value, fmt)
+        target_opcode = self.read_and_unpack('B')
+        actual_opcode = self.read_and_unpack('B')
+        actual_value = self.read_and_unpack(fmt)
+        logger.debug(f'Board response ({target_opcode}, {actual_opcode}, {actual_value})...')
+        if actual_opcode != opcode.value or actual_value != value:
+            raise RuntimeError(f'Write/read mismatch in {self.__class__.__name__}.write_cmd()')
+
+    def setup_analog_sampling_sketch(self, pin_list: tuple, sampling_interval: int) -> None:
+        """ Setup the sktchAnalogSampling sketch.
+        """
+        self.write_cmd(OpCode.OP_CODE_SELECT_NUM_ANALOG_PINS, len(pin_list), 'B')
+        for pin in pin_list:
+            self.write_cmd(OpCode.OP_CODE_SELECT_ANALOG_PIN, pin, 'B')
+        self.write_cmd(OpCode.OP_CODE_SELECT_SAMPLING_INTERVAL, sampling_interval, 'I')
 
 
 
