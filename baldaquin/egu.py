1404e72cbbf2586fbd3df4a26cea1c112e5ed859
Luca Baldini <luca.baldini@pi.infn.it>
2024-12-25 10:34:19 +0100

    Started implementing the conversion module.

--- baldaquin/egu.py
+++ baldaquin/egu.py
@@ -15,3 +15,68 @@
 
 """Engineering units and converters.
 """
+
+import numpy as np
+
+
+class ConversionBase:
+
+    """Abstract base class for a generic conversion.
+    """
+
+    def _conversion_function(self, raw) -> None:
+        """Conversion function, to be reimplemented in derived classes.
+        """
+        raise NotImplementedError
+
+    def __call__(self, raw):
+        """Special dunder method for executing the actual conversion.
+        """
+        return self._conversion_function(raw)
+
+
+
+class LinearConversion(ConversionBase):
+
+    """Linear conversion.
+    """
+
+    def __init__(self, slope: float, intercept: float = 0) -> None:
+        """Constructor.
+        """
+        self.slope = slope
+        self.intercept = intercept
+
+    def _conversion_function(self, raw):
+        """Overloaded method.
+        """
+        return self.slope * raw + self.intercept
+
+
+
+class PolynomialConversion(ConversionBase):
+
+    """Polynomial conversion.
+    """
+
+    pass
+
+
+
+class SplineConversion(ConversionBase):
+
+    """Polynomial conversion.
+    """
+
+    def __init__(self, x, y, k: int = 3) -> None:
+        """Constructor.
+        """
+        pass
+
+
+
+if __name__ == '__main__':
+    c = LinearConversion(2., 1.)
+    raw = np.linspace(0., 1., 11)
+    val = c(raw)
+    print(raw, val)
