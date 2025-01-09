413e556962b97a2c83d3998622f4e5da864188ea
Luca Baldini <luca.baldini@pi.infn.it>
2025-01-09 17:13:59 +0100

    Minor.

--- baldaquin/__init__.py
+++ baldaquin/__init__.py
@@ -13,9 +13,10 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-
 """System-wide facilities.
 """
+
+from __future__ import annotations
 
 import os
 from pathlib import Path
