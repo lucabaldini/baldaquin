f84480ac8d8e7340a2b08a98bce8ea3847cf27dd
Luca Baldini <luca.baldini@pi.infn.it>
2025-01-23 22:48:26 +0100

    Initial import.

---
+++ baldaquin/plasduino/apps/plasduino_pendulum.py
@@ -0,0 +1,82 @@
+# Copyright (C) 2024 the baldaquin team.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+"""Plasduino pendulum application.
+"""
+
+from pathlib import Path
+
+from baldaquin import plasduino
+from baldaquin.__qt__ import QtWidgets
+from baldaquin.buf import WriteMode
+from baldaquin.gui import bootstrap_window, MainWindow, SimpleControlBar
+from baldaquin.pkt import AbstractPacket
+from baldaquin.plasduino import PLASDUINO_APP_CONFIG
+from baldaquin.plasduino.common import PlasduinoRunControl, PlasduinoDigitalEventHandler, \
+    PlasduinoAnalogConfiguration, PlasduinoDigitalUserApplicationBase
+from baldaquin.plasduino.protocol import COMMENT_PREFIX, TEXT_SEPARATOR, DigitalTransition
+
+
+class AppMainWindow(MainWindow):
+
+    """Application graphical user interface.
+    """
+
+    _PROJECT_NAME = plasduino.PROJECT_NAME
+    _CONTROL_BAR_CLASS = SimpleControlBar
+
+
+class Pendulum(PlasduinoDigitalUserApplicationBase):
+
+    """Simplest possible user application for testing purposes.
+    """
+
+    NAME = 'Pendulum'
+    CONFIGURATION_CLASS = PlasduinoAnalogConfiguration
+    CONFIGURATION_FILE_PATH = PLASDUINO_APP_CONFIG / 'plasduino_pendulum.cfg'
+    EVENT_HANDLER_CLASS = PlasduinoDigitalEventHandler
+
+    @staticmethod
+    def text_header() -> str:
+        """Return the header for the output text file.
+        """
+        return f'{AbstractPacket.text_header()}\n' \
+               f'{COMMENT_PREFIX}Time [s]{TEXT_SEPARATOR}Edge type\n'
+
+    def transition_to_text(self, transition: DigitalTransition) -> str:
+        """Convert a temperature readout to text for use in a custom sink.
+        """
+        return f'{transition.seconds:.6f}{TEXT_SEPARATOR}{transition.edge}\n'
+
+    def configure(self) -> None:
+        """Overloaded method.
+        """
+
+    def pre_start(self) -> None:
+        """Overloaded method.
+        """
+        file_path = Path(f'{self.current_output_file_base}_data.txt')
+        self.event_handler.add_custom_sink(file_path, WriteMode.TEXT, self.transition_to_text,
+                                           self.text_header())
+
+    def process_packet(self, packet_data: bytes) -> AbstractPacket:
+        """Overloaded method.
+        """
+        transition = DigitalTransition.unpack(packet_data)
+        return transition
+
+
+if __name__ == '__main__':
+    bootstrap_window(AppMainWindow, PlasduinoRunControl(), Pendulum())
