1afdc3f15e1226c7b5477355c85083e035cc7802
Luca Baldini <luca.baldini@pi.infn.it>
2025-01-30 17:27:39 +0100

    New windows .bat file inspired from gpdsw

--- setup.bat
+++ setup.bat
@@ -1,1 +1,12 @@
-$env:PYTHONPATH = $pwd
+@ECHO OFF
+REM See this stackoverflow question
+REM http:\\stackoverflow.com\questions\3827567\how-to-get-the-path-of-the-batch-script-in-windows
+REM for the magic in this command
+SET WIN_SETUP_DIR=%~dp0
+SET SETUP_DIR=%WIN_SETUP_DIR:\=/%
+
+SET BALDAQUIN_ROOT=%SETUP_DIR:~0,-1%
+ECHO "BALDAQUIN_ROOT set to " %BALDAQUIN_ROOT%
+
+SET PYTHONPATH=%BALDAQUIN_ROOT%;%PYTHONPATH%
+ECHO "PYTHONPATH set to " %PYTHONPATH%
