4d795004f9e37a2fecb2cdf10f37a46383e5ab78
Luca BALDINI <lbaldini@desktop.pi.infn.it>
2025-01-31 13:27:26 +0100

    Updated.

--- setup.bat
+++ setup.bat
@@ -1,12 +1,11 @@
 @ECHO OFF
-REM See this stackoverflow question
-REM http:\\stackoverflow.com\questions\3827567\how-to-get-the-path-of-the-batch-script-in-windows
-REM for the magic in this command
-SET WIN_SETUP_DIR=%~dp0
-SET SETUP_DIR=%WIN_SETUP_DIR:\=/%
 
-SET BALDAQUIN_ROOT=%SETUP_DIR:~0,-1%
-ECHO "BALDAQUIN_ROOT set to " %BALDAQUIN_ROOT%
+:: Get the current directory (PWD)
+set "BALDAQUIN_ROOT=%CD%"
 
-SET PYTHONPATH=%BALDAQUIN_ROOT%;%PYTHONPATH%
-ECHO "PYTHONPATH set to " %PYTHONPATH%
+:: Prepend the current directory to PYTHONPATH
+set "PYTHONPATH=%BALDAQUIN_ROOT%;%PYTHONPATH%"
+
+:: Print the new PYTHONPATH for verification
+echo BALDAQUIN_ROOT: %BALDAQUIN_ROOT%
+echo Updated PYTHONPATH: %PYTHONPATH%
