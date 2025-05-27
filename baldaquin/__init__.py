bc3f1f31c07f25214d73bc8f6bc36f9f64c1fd9f
Luca Baldini <luca.baldini@pi.infn.it>
2025-05-27 13:44:56 +0200

    Minor.

--- baldaquin/__init__.py
+++ baldaquin/__init__.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2022--2023 the baldaquin team.
+# Copyright (C) 2022--2025 the baldaquin team.
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -43,6 +43,9 @@ def start_message() -> None:
     Visit https://github.com/lucabaldini/baldaquin for more information.
     """
     print(msg)
+
+
+start_message()
 
 
 def reset_logger(level: str = 'DEBUG') -> int:
